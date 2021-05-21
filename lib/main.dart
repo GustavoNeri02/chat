bba2d1e4b51f87bf884afa30478547c9af17655c
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2021-05-21 20:47:17 -0300

    Criado widget barra de envios: *IconButton -> IconThemeData(color: Colors.blue) *TextField -> InputDecoration.collapsed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:chat/chat_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() async{
-  runApp(MyApp());
+  runApp(MaterialApp(home: MyApp()));
   Firestore.instance.collection("mensagens").document("msg1").snapshots().listen((event) {
     print(event.data);
   });
@@ -16,8 +17,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(color: Colors.blue)
       ),
-      home: Container(),
+      home: ChatScreen(),
     );
   }
 }
