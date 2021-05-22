151de923ab3e50cbef56e7848eaefb154e7c2f99
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2021-05-21 21:32:08 -0300

    Enviando texto para Firebase: *controle de envio de mensagem -> em IconButton(onPressed: null -> não clicável)

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chat/text_composer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -9,6 +10,10 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  void _sendMessages(String text) {
+    Firestore.instance.collection("mensagens").add({"text": text});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +22,7 @@ class _ChatScreenState extends State<ChatScreen> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: TextComposer(),
+      body: TextComposer(_sendMessages),
     );
   }
 }
--- lib/text_composer.dart
+++ lib/text_composer.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 
 class TextComposer extends StatefulWidget {
-  const TextComposer({Key key}) : super(key: key);
+  Function(String) sendMessage;
+  TextComposer(this.sendMessage);
 
   @override
   _TextComposerState createState() => _TextComposerState();
 }
 
 class _TextComposerState extends State<TextComposer> {
+  TextEditingController textEditingController = TextEditingController();
+
   bool _isComposing = false;
+
+  void _resetTextField() {
+    setState(() {
+      textEditingController.clear();
+      this._isComposing = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -18,6 +29,7 @@ class _TextComposerState extends State<TextComposer> {
           IconButton(icon: Icon(Icons.camera), onPressed: () {}),
           Expanded(
             child: TextField(
+              controller: textEditingController,
               decoration:
                   InputDecoration.collapsed(hintText: "Enviar uma mensagem..."),
               onChanged: (text) {
@@ -25,14 +37,21 @@ class _TextComposerState extends State<TextComposer> {
                   _isComposing = text.isNotEmpty;
                 });
               },
-              onSubmitted: (text) {},
+              onSubmitted: (text) {
+                widget.sendMessage(textEditingController.text);
+                _resetTextField();
+              },
             ),
           ),
           IconButton(
-              icon: Icon(Icons.send),
-              onPressed: () {
-                _isComposing ? () {} : null;
-              })
+            icon: Icon(Icons.send),
+            onPressed: _isComposing
+                ? () {
+                    widget.sendMessage(textEditingController.text);
+                    _resetTextField();
+                  }
+                : null,
+          )
         ],
       ),
     );
