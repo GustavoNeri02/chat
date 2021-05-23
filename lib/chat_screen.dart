4c7bbece6d48cea6e2b80f766b5360674c7b458f
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2021-05-23 18:46:55 -0300

    Enviando Imagem para Firebase: *Uso de ImagePicker *map dependendo do envio (text:, imgUrl:)

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:chat/text_composer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -10,8 +13,23 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  void _sendMessages(String text) {
-    Firestore.instance.collection("mensagens").add({"text": text});
+  void _sendMessages({String text, File imgFile}) async {
+    Map<String, dynamic> map = {};
+    if (imgFile != null) {
+      StorageUploadTask task = FirebaseStorage.instance
+          .ref()
+          .child("imagens")
+          .child(DateTime.now().millisecondsSinceEpoch.toString())
+          .putFile(imgFile);
+      StorageTaskSnapshot taskSnapshot = await task.onComplete;
+      String url = await taskSnapshot.ref.getDownloadURL();
+      map["imageUrl"] = url;
+    }
+    if(text != null){
+      map["text"] = text;
+    }
+
+    Firestore.instance.collection("mensagens").add(map);
   }
 
   @override
--- lib/text_composer.dart
+++ lib/text_composer.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class TextComposer extends StatefulWidget {
-  Function(String) sendMessage;
+  final Function({String text, File imgFile}) sendMessage;
   TextComposer(this.sendMessage);
 
   @override
@@ -26,7 +29,15 @@ class _TextComposerState extends State<TextComposer> {
       margin: EdgeInsets.symmetric(horizontal: 8),
       child: Row(
         children: [
-          IconButton(icon: Icon(Icons.camera), onPressed: () {}),
+          IconButton(
+              icon: Icon(Icons.camera),
+              onPressed: () async{
+                final PickedFile imgPickedFile = await ImagePicker().getImage(source: ImageSource.gallery);
+                final File imgFile = File(imgPickedFile.path);
+                if( imgFile != null){
+                  widget.sendMessage(imgFile: imgFile);
+                }
+              }),
           Expanded(
             child: TextField(
               controller: textEditingController,
@@ -38,7 +49,7 @@ class _TextComposerState extends State<TextComposer> {
                 });
               },
               onSubmitted: (text) {
-                widget.sendMessage(textEditingController.text);
+                widget.sendMessage(text: textEditingController.text);
                 _resetTextField();
               },
             ),
@@ -47,7 +58,7 @@ class _TextComposerState extends State<TextComposer> {
             icon: Icon(Icons.send),
             onPressed: _isComposing
                 ? () {
-                    widget.sendMessage(textEditingController.text);
+                    widget.sendMessage(text: textEditingController.text);
                     _resetTextField();
                   }
                 : null,
